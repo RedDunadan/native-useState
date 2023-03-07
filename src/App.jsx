a1be18118a31967206313076d27a6635cd49dbd5
Rhedd <devrhedd@gmail.com>
2023-03-07 19:41:45 -0300

    working on states

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  let [count, setCount] = useState(10);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={addOne} >Count = {count}</button>
+    </div>
+  )
+}
+
+export default App
